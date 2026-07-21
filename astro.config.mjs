cbfcfd4494fbc44cc73c8130ab10af764a5b3a93
root <root@ubuntu-cloudpanel.localhost>
2026-07-21 20:15:14 +0000

    Migra hub para rede.loopsbrasil.com.br (loopsbrasil.com.br fica livre pra outra coisa)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://loopsbrasil.com.br',
+  site: 'https://rede.loopsbrasil.com.br',
   integrations: [tailwind(), sitemap()],
 });
